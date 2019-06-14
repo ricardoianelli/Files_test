980917ef312456b55524c0991d97b8231af6070e
Ricardo Ianelli <ricardoianelli@hotmail.com>
2019-06-14 14:32:58 -0300

    Organizando classe principal do programa

--- Teste_arquivos/Program.cs
+++ Teste_arquivos/Program.cs
@@ -8,7 +8,34 @@ namespace Teste_arquivos
     {
         static void Main(string[] args)
         {
-            
+            string inPath = @"c:\temp\sales";
+            string outPath = inPath + @"\out";
+
+            try
+            {
+                if (!Directory.Exists(outPath))
+                    Directory.CreateDirectory(outPath);
+
+                using (StreamReader sr = new StreamReader(inPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        // ler produtos, criar produtos e vendas
+                    }
+                }
+
+                using (StreamWriter sw = new StreamWriter(outPath))
+                {
+                    // Salvar summary no arquivo "\out\summary.csv"
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            Console.ReadLine();
         }
  
     }
