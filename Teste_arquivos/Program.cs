8b072b028d544c71e6e232da2ffa19c2141e6b1c
Ricardo Ianelli <ricardoianelli@hotmail.com>
2019-06-14 18:15:55 -0300

    The user now insert the csv file path at the console

--- Teste_arquivos/Program.cs
+++ Teste_arquivos/Program.cs
@@ -9,16 +9,23 @@ namespace Teste_arquivos
     {
         static void Main(string[] args)
         {
-            string inPath = @"c:\temp\sales";
-            string outPath = inPath + @"\out";
-            Sale sale = new Sale();
-
             try{
-                if (!Directory.Exists(outPath)){
-                    Directory.CreateDirectory(outPath);
+
+                Sale sale = new Sale();
+                Console.WriteLine("Enter the source csv file path: ");
+                string inPath = Console.ReadLine();
+                string inFolder = Path.GetDirectoryName(inPath);
+
+                if (!Directory.Exists(inFolder)){
+                    throw new IOException("The file path does not exist!");
                 }
 
-                using (StreamReader reader = new StreamReader(inPath+ @"\sales.csv")){
+                string outFolder = inFolder + @"\out";
+                if (!Directory.Exists(outFolder)){
+                    Directory.CreateDirectory(outFolder);
+                }
+
+                using (StreamReader reader = new StreamReader(inPath)){
                     while (!reader.EndOfStream){
                         string[] product = reader.ReadLine().Split(",");
                         ProductSale currentSale = new ProductSale(new Product(product[0], double.Parse(product[1], CultureInfo.InvariantCulture)), int.Parse(product[2]));
@@ -26,12 +33,12 @@ namespace Teste_arquivos
                     }
                 }
 
-                using (StreamWriter writer = new StreamWriter(outPath+ @"\summary.csv")){
+                using (StreamWriter writer = new StreamWriter(outFolder + @"\summary.csv")){
                     foreach (string saleString in sale.generateSummary()){
                         writer.WriteLine(saleString);
                     }
                 }
-                Console.WriteLine("Summary.csv generated successfully at " + outPath + ".");
+                Console.WriteLine("Summary.csv generated successfully at " + outFolder + ".");
             }
             catch (IOException exception){
                 Console.WriteLine("An IO error occurred: " + exception.Message);
