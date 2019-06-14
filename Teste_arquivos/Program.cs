c5d7c8c604f3c5dfd2af7d0597146ae6ba02dac9
Ricardo Ianelli <ricardoianelli@hotmail.com>
2019-06-14 14:44:38 -0300

    Implementando a leitura inicial dos produtos no arquivo csv

--- Teste_arquivos/Program.cs
+++ Teste_arquivos/Program.cs
@@ -16,21 +16,29 @@ namespace Teste_arquivos
                 if (!Directory.Exists(outPath))
                     Directory.CreateDirectory(outPath);
 
-                using (StreamReader sr = new StreamReader(inPath))
+                using (StreamReader sr = new StreamReader(inPath+ @"\sales.csv"))
                 {
                     while (!sr.EndOfStream)
                     {
-                        // ler produtos, criar produtos e vendas
+                        string[] product = sr.ReadLine().Split(",");
+                        Console.WriteLine("\nProduct:");
+                        Console.WriteLine("Name: " + product[0]);
+                        Console.WriteLine("Price: " + product[1]);
+                        Console.WriteLine("Quantity: " + product[2]);
                     }
                 }
 
-                using (StreamWriter sw = new StreamWriter(outPath))
+                using (StreamWriter sw = new StreamWriter(outPath+ @"\summary.csv"))
                 {
                     // Salvar summary no arquivo "\out\summary.csv"
                 }
 
             }
             catch (IOException e)
+            {
+                Console.WriteLine("An IO error occurred: " + e.Message);
+            }
+            catch (Exception e)
             {
                 Console.WriteLine("An error occurred: " + e.Message);
             }
